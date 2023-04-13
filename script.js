71e9a3d9c15e8d81a8252efc1cf2b8ed1adc43a5
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-13 18:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // complete this js code
 function Person(name, age) {
+	constructor{
+		this._name = name;
+		this._age = age;
+	}
 	greet(){
-		console.log("Hello, my name is "+name+", I am "+age+" years old.")
+		console.log("Hello, my name is "+this._name+", I am "+this._age+" years old.")
 	}
 }
 
 function Employee extends Person(name, age, jobTitle) {
 	constructor(name, age, jobTitle){
-		this.name = name;
-		this.age = age;
-		this.jobTtitle = jobTitle
+		this._name = name;
+		this._age = age;
+		this._jobTtitle = jobTitle
 	}
 	jobGreet(){
 		console.log("Hello, my name is "+name+," I am "+age+" years old, and my job title is "+jobTitle+".")
